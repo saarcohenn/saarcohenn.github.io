23e6e8c7241d4596fe090649fbf3fe85bb1b341b
saar cohen <saar.cohenn@gmail.com>
2021-05-02 19:07:50 +0300

    [Added] - About, Projects and Skills routing was added.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Switch, Route, useLocation } from "react-router-dom";
+
+// Components
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Nav */}
+      <GlobalStyle />
+      <Nav />
+      <Switch location={location} key={location.pathname}>
+        {/* About */}
+        <Route exact path="/">
+          <About />
+        </Route>
+        {/* Projects */}
+        <Route exact path="/projects">
+          <Projects />
+        </Route>
+        {/* Skills */}
+        <Route exact path="/skills">
+          <Skills />
+        </Route>
+        {/* Footer */}
+        <Route exact path="/footer">
+          <Footer />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import styled from "styled-components";
+import { Link, useLocation } from "react-router-dom";
+
+const Nav = () => {
+  const location = useLocation();
+  return (
+    <StyledNav>
+      <h1>
+        <Link to="/" id="logo">
+          Saar Cohen
+        </Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">About</Link>
+        </li>
+        <li>
+          <Link to="/Projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/Skills">Skills</Link>
+        </li>
+      </ul>
+    </StyledNav>
+  );
+};
+
+const StyledNav = styled.nav`
+  min-height: 5vh;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 10rem;
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  ul {
+    display: flex;
+    list-style: none;
+  }
+  li {
+    padding-left: 10rem;
+    position: relative;
+  }
+  #logo {
+    font-size: 1.8rem;
+    font-family: "lobster", cursive;
+    font-weight: lighter;
+  }
+  @media (max-width: 1300) {
+    flex-direction: column;
+    padding: 2rem 1rem;
+    ul {
+      padding: 2rem;
+      justify-content: space-around;
+      width: 100%;
+    }
+    li {
+      padding: 0;
+    }
+    #logo {
+      display: inline-block;
+      margin: 2rem;
+    }
+  }
+`;
+export default Nav;
